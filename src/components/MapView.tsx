0292e7f4008a5fe535d21ca4046d17f8bcc62297
Ahmad Firdaus <el.vierda@gmail.com>
2025-11-27 13:26:07 +0700

    Updated package-lock.json

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -15,6 +15,8 @@ import { REPORT_TYPES, ReportType } from '../types';
 import { useAuth } from '../contexts/AuthContext';
 import { validateCoordinates, validateDescription } from '../utils/validation';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 const DEFAULT_CENTER: LatLngTuple = [-6.597, 106.799];
 const DEFAULT_ZOOM = 12;
